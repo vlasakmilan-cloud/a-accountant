2a0f29d1c275d86afa3837f326228b5d0a391b52
vlasakmilan-cloud <vlasakmilan@gmail.com>
2025-06-25 22:49:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ['qezbrqjqbojmonmoefiu.supabase.co'],
-  },
+  // Odstranili jsme deprecated appDir
 }
 
 module.exports = nextConfig
